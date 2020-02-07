1ca984431d5377b661fedfc4ff4c8807153b9d9e
Paul Soule <22938608+pds2208@users.noreply.github.com>
2020-02-07 19:18:39 +0000

    WIP

--- compare.py
+++ compare.py
@@ -18,15 +18,12 @@ def get_datasets(sas_survey_output, py_survey_output):
     sas_survey_df = pd.read_csv(sas_survey_output, engine='python', na_values=' ')
     py_survey_df = pd.read_csv(py_survey_output, engine='python', na_values=' ')
 
-    # Columns to upper string
     py_survey_df.columns = py_survey_df.columns.str.upper()
     sas_survey_df.columns = sas_survey_df.columns.str.upper()
 
-    # Extract columns
     py_survey_df = py_survey_df[columns_to_extract]
     sas_survey_df = sas_survey_df[columns_to_extract]
 
-    # Export to csv
     return sas_survey_df, py_survey_df
 
 
@@ -35,19 +32,30 @@ def is_equal(a, b):
 
 
 def get_differences(sas_output, ips_output):
-    sas_output.replace("", np.nan, inplace=True)
-    ips_output.replace("", np.nan, inplace=True)
-    sas_output.sort_values(by=['SERIAL'], axis=1, inplace=True)
-    ips_output.sort_values(by=['SERIAL'], axis=1, inplace=True)
+    # sas_output.replace("", np.nan, inplace=True)
+    # ips_output.replace("", np.nan, inplace=True)
+
+    sas_output.sort_values(by=['SERIAL'], inplace=True)
+    ips_output.sort_values(by=['SERIAL'], inplace=True)
+
+    sas_output.reset_index(inplace=True)
+    ips_output.reset_index(inplace=True)
 
     for a in columns_to_extract:
+        sas_output.fillna(0, inplace=True)
+        ips_output.fillna(0, inplace=True)
         sas_output['SAS_' + a] = sas_output[a]
         sas_output['IPS_' + a] = ips_output[a]
         sas_output[a + "_Match"] = np.where(is_equal(sas_output[a], ips_output[a]), True, False)
 
+        def perc(x, y):
+            if x.ge(y):
+                return abs(x - y) / ((x + y) / 2)
+            return abs(y - x) / ((x + y) / 2)
+
         if sas_output[a].dtypes == "float64":
-            sas_output[a + "_Diff"] = np.where(is_equal(sas_output[a], ips_output[a]), 0,
-                                           (sas_output[a] - ips_output[a]))
+            sas_output[a + "_Diff %"] = \
+                np.where(is_equal(sas_output[a], ips_output[a]), 0, perc(sas_output[a], ips_output[a]))
         else:
             sas_output[a + "_Diff"] = np.where(is_equal(sas_output[a], ips_output[a]), "", "False")
 
@@ -56,14 +64,12 @@ def get_differences(sas_output, ips_output):
     query = ' | '.join(map(lambda x: x + '_Match' + " == False", columns_to_extract))
     out = sas_output.query(query)
 
-    # for a in columns_to_extract:
-    #     del out[a + "_Match"]
-
     return out
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog="compare", description="Compare SAS and IPS files and write difference to CSV file")
+    parser = argparse.ArgumentParser(prog="compare",
+                                     description="Compare SAS and IPS files and write difference to CSV file")
     parser.add_argument("-s", "--sfile", dest="sas_output", required=True,
                         help="SAS file to compare IPS file against")
     parser.add_argument("-i", "--ifile", dest="ips_output", required=True,
@@ -83,18 +89,18 @@ if __name__ == "__main__":
 
     differences = get_differences(df1, df2)
 
+
     def highlight_max(s):
         is_max = s == False
         return ['background-color: yellow' if v else '' for v in is_max]
 
+
     match = []
-    l = list(differences.columns.values)
-    for a in l:
+    for a in list(differences.columns.values):
         if a.endswith("_Match"):
             match.append(a)
-    differences.style.\
-        apply(highlight_max, subset=match).\
-        to_excel(differences_file, engine='xlsxwriter', index=False, freeze_panes=(1, 9999))
+    differences.style. \
+        apply(highlight_max, subset=match). \
+        to_excel(differences_file, sheet_name="Differences", engine='xlsxwriter', index=False, freeze_panes=(1, 1))
 
-    # differences.to_excel(differences_file, engine='xlsxwriter', freeze_panes=(1, 9999), index=False)
     print("All done. Differences are in " + differences_file)
